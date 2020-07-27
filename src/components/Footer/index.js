d70833a392fef93c788cf577a33934b4845e4186
italoribeiroc <italoribeiro@estudante.ufscar.br>
2020-07-27 13:46:48 -0300

    primeiro commit

---
+++ src/components/Footer/index.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import { FooterBase } from './styles';
+
+function Footer() {
+  return (
+    <FooterBase>
+      <p>
+        Desenvolvido por Italo Ribeiro durante a imersão React
+      </p>
+    </FooterBase>
+  );
+}
+
+export default Footer;
